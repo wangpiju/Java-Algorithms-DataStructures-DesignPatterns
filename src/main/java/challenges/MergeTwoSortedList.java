4500df10668ca1ac4212ff18e234c76053aa1956
jess.wang <jess.wang@ace.io>
2021-07-14 18:46:46 +0800

    fix

--- src/main/java/challenges/MergeTwoSortedList.java
+++ src/main/java/challenges/MergeTwoSortedList.java
@@ -27,6 +27,7 @@ public class MergeTwoSortedList {
     }
   }
 
+  //recursion
   public static ListNode mergeTwoLists(ListNode l1, ListNode l2) {
     if (l1 == null) {
       return l2;
@@ -45,16 +46,54 @@ public class MergeTwoSortedList {
     }
   }
 
+  //loop
+  public static ListNode mergeTwoLists2(ListNode l1, ListNode l2) {
+    //fake head
+    ListNode head = new ListNode(0);
+    ListNode p = head;
+
+    ListNode p1 = l1;
+    ListNode p2 = l2;
+
+    //The key to solve the problem is defining a fake head. Then compare the first elements from each list. Add the smaller one to the merged list.
+    while (p1 != null && p2 != null) {
+      if (p1.val < p2.val) {
+        p.next = p1;
+        p1 = p1.next;
+      } else {
+        p.next = p2;
+        p2 = p2.next;
+      }
+
+      p = p.next;
+    }
+
+    //when one of them is empty, simply append it to the merged list
+    if (p1 != null) {
+      p.next = p1;
+    }
+
+    if (p2 != null) {
+      p.next = p2;
+    }
+
+    return head.next;
+  }
+
   public static void main(String[] args) {
     ListNode a = new ListNode(1);
     a.next = new ListNode(2);
     a.next.next = new ListNode(4);
+    a.next.next.next = new ListNode(6);
+    System.out.println("list a: " + a);
     ListNode b = new ListNode(1);
     b.next = new ListNode(3);
     b.next.next = new ListNode(4);
+    b.next.next.next = new ListNode(7);
+    System.out.println("list b: " + b);
 
-    ListNode result = mergeTwoLists(a, b);
+    ListNode result = mergeTwoLists2(b, a);
 
-    System.out.println(result.toString());
+    System.out.println("result: " + result.toString());
   }
 }
